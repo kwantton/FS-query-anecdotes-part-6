14e1810207eb41f64caea6a173883765a5ab5175
Antton Kasslin <antton.kasslin@hotmail.com>
2024-07-04 21:45:19 +0300

    6.24 done (error handling in case of too short an anecdote etc; utilizing onError of AnecdoteForm). PART 6 DONE

--- src/components/AnecdoteForm.jsx
+++ src/components/AnecdoteForm.jsx
@@ -1,16 +1,30 @@
 import { createAnecdote } from '../requests'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { useContext } from "react"                                              // 6.23
-import NotificationContext from "../NotificationContext"                         // 6.23
+import { useContext } from "react"                                                  // 6.23
+import NotificationContext from "../NotificationContext"                            // 6.23
 
 const AnecdoteForm = () => {
-  const [notification, notificationDispatch] = useContext(NotificationContext)  // 6.23
+  const [notification, notificationDispatch] = useContext(NotificationContext)      // 6.23
   const newAnecdoteMutation = useMutation({ 
     mutationFn: createAnecdote,
     onSuccess: (newAnecdote) => {
       const anecdotes = queryClient.getQueryData(['anecdotes'])      
       queryClient.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote))
+
+      notificationDispatch({                                                        // 6.23. 6.24: check that this works only if successful
+        type:'NOTIFICATION', 
+        payload: `new anecdote "${newAnecdote.content}" added!`
+      })
+      setTimeout(() => {notificationDispatch({type:"NOTIFICATION",payload:''})},5000)
     },
+    
+    onError: () => {                                                                // 6.24
+      notificationDispatch({
+        type:'NOTIFICATION', 
+        payload: `length too short: please write more than 5 characters (example: "shit happens" instead of "shit")`
+      })
+      setTimeout(() => {notificationDispatch({type:"NOTIFICATION",payload:''})},5000)
+    }                                                              // 6.24; if it fails (less than 5 char -> error, or other problem), then...
   })
   const queryClient = useQueryClient()  
 
@@ -20,11 +34,7 @@ const onCreate = async (event) => {
   event.target.anecdote.value = ''
   newAnecdoteMutation.mutate({ content, votes: 0 })  
   
-  notificationDispatch({                                                        // 6.23
-    type:'NOTIFICATION', 
-    payload: `new anecdote "${content}" added!`
-  })
-  setTimeout(() => {notificationDispatch({type:"NOTIFICATION",payload:''})},5000)
+  
 }
 
   return (
